ced7f53088eb709009404d4a6df799ebf76e6ec5
neel <neel12@somaiya.edu>
2018-07-19 01:33:49 +0530

    about, schedule, faw added

--- js/index.js
+++ js/index.js
@@ -6,3 +6,9 @@ $('.q').on('click', function(){
   $(this).children('.shape').toggleClass('opened');
   $(this).siblings().children('.ans').toggleClass('show');
 })
+
+$(window).scroll(function() {
+  var theta = $(window).scrollTop() / 100 % Math.PI;
+  $('.backanimation').css({ transform: 'rotate(' + theta + 'rad)' });
+  // $('#rightgear').css({ transform: 'rotate(-' + theta + 'rad)' });
+  });
