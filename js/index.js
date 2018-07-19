ac509411689c1edc027fcb78c9333c7642b1df3f
neel <neel12@somaiya.edu>
2018-07-20 03:01:49 +0530

    update

--- js/index.js
+++ js/index.js
@@ -9,6 +9,13 @@ $('.q').on('click', function(){
 
 $(window).scroll(function() {
   var theta = $(window).scrollTop() / 100 % Math.PI;
-  $('.backanimation').css({ transform: 'rotate(' + theta + 'rad)' });
+  theta = theta/2;
+  var travel = $(window).scrollTop();
+  $('.backanimation__rectangle--1').css({ transform: 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)'});
+  $('.backanimation__rectangle--2').css({ transform: 'translateY(-' + (travel/8) +'px)' + 'rotate(' + (theta) + 'rad)' });
+  $('.backanimation__rectangle--3').css({ transform: 'rotate(' + (theta-100) + 'rad)' });
+  $('.backanimation__circle--1').css({ transform: 'rotate(' + (theta+100) + 'rad)' });
+  $('.backanimation__circle--2').css({ transform: 'rotate(' + (theta) + 'rad)' });
+  $('.backanimation__circle--3').css({ transform: 'rotate(' + (theta-100) + 'rad)' });
   // $('#rightgear').css({ transform: 'rotate(-' + theta + 'rad)' });
   });
