aff9db71af29d2fa48c440da5a10d0908ac1b297
freelancer-98 <neel12@somaiya.edu>
2018-07-24 00:38:07 +0530

    backanimation init

--- js/index.js
+++ js/index.js
@@ -7,33 +7,33 @@ $('.q').hover (function(){
     $(this).siblings().children('.ans').toggleClass('show');
   })
 
-// var window = document.getElementsByTagName("window")[0];
-// var rect1 = document.getElementsByClassName("backanimation__rectangle--1")[0];
-// var rect2 = document.getElementsByClassName("backanimation__rectangle--2")[0];
-// var rect3 = document.getElementsByClassName("backanimation__rectangle--3")[0];
-// var rect4 = document.getElementsByClassName("backanimation__rectangle--4")[0];
+var window = document.getElementsByTagName("window")[0];
+var rect1 = document.getElementsByClassName("backanimation__rectangle--1")[0];
+var rect2 = document.getElementsByClassName("backanimation__rectangle--2")[0];
+var rect3 = document.getElementsByClassName("backanimation__rectangle--3")[0];
+var rect4 = document.getElementsByClassName("backanimation__rectangle--4")[0];
 
-// var circ1 = document.getElementsByClassName("backanimation__circle--1")[0];
-// var circ2 = document.getElementsByClassName("backanimation__circle--2")[0];
-// var circ3 = document.getElementsByClassName("backanimation__circle--3")[0];
-// var circ4 = document.getElementsByClassName("backanimation__circle--4")[0];
+var circ1 = document.getElementsByClassName("backanimation__circle--1")[0];
+var circ2 = document.getElementsByClassName("backanimation__circle--2")[0];
+var circ3 = document.getElementsByClassName("backanimation__circle--3")[0];
+var circ4 = document.getElementsByClassName("backanimation__circle--4")[0];
 
-// (window).addEventListener("scroll",function() {
-//   var theta = (window).scrollY / 100 % Math.PI;
-//   theta = theta/2;
-//   var travel = (window).scrollY;
+(window).addEventListener("scroll",function() {
+  var theta = (window).scrollY / 100 % Math.PI;
+  theta = theta/2;
+  var travel = (window).scrollY;
 
-//   // console.log(theta);
-//   rect1.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
-//   rect2.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
-//   rect3.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
-//   rect4.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
-//   circ1.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
-//   circ2.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
-//   circ3.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
-//   circ4.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
+  // console.log(theta);
+  rect1.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
+  rect2.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
+  rect3.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
+  rect4.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
+  circ1.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
+  circ2.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
+  circ3.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
+  circ4.style.transform = 'translateY(' + (travel/10) +'px)' + 'rotate(' + (theta+100) + 'rad)';
   
-// });
+});
 
 //   // TODO
 //   // 1. Convert JQuery to JS
