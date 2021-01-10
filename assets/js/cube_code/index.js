3d30bbcfdf0c585c194c04b82e24dac2d0b0ac44
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-01-10 19:55:57 +0530

    Remove JS Cube for now, Added Cube GIF

--- assets/js/cube_code/index.js
+++ assets/js/cube_code/index.js
@@ -1,8 +1,9 @@
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three'
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, Color } from 'three'
 
 const container = document.getElementById("cube")
 
 const scene = new Scene();
+scene.background = new Color("#ffccbb")
 const camera = new PerspectiveCamera( 75, container.clientWidth / container.clientHeight, 0.1, 1000 );
 
 const renderer = new WebGLRenderer();
@@ -10,7 +11,7 @@ renderer.setSize( container.clientWidth, container.clientHeight );
 container.appendChild( renderer.domElement );
 
 const geometry = new BoxGeometry(2, 2, 2);
-const material = new MeshBasicMaterial( { color: 0x00ffdd } );
+const material = new MeshBasicMaterial( { color: 0x555555 } );
 const cube = new Mesh( geometry, material );
 scene.add( cube );
 
@@ -22,7 +23,6 @@ function animate() {
     
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-    camera.position.z += 0.01;
 
 	renderer.render( scene, camera );
 }
