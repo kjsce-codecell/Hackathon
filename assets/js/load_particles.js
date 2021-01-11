1ca553b8928bc0383371f735d2af7f3af3aff0b6
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-01-11 10:16:09 +0530

    Add particle.js

---
+++ assets/js/load_particles.js
@@ -0,0 +1,5 @@
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load("particles-js", "../assets/js/particles.json", function () {
+    console.log("callback - particles.js config loaded");
+});
