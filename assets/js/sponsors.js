fed63f8d9ac2b720605e7ed4d14b4f7e9f8e2140
Naman Shah <nashah2001@gmail.com>
2021-01-12 12:03:33 +0530

    functional js added, minor changes left

--- assets/js/sponsors.js
+++ assets/js/sponsors.js
@@ -1,13 +1,27 @@
+// $("#prev-sp").click(function(){
+// 	$("#curr-sp").removeClass("clicked-button");
+// 	$("#prev-sp").addClass("clicked-button");
+// 	$(".previous-sponsors").removeClass("hidden");
+// 	$(".current-sponsors").addClass("hidden");
+// });
+
+// $("#curr-sp").click(function(){
+// 	$("#prev-sp").removeClass("clicked-button");
+// 	$("#curr-sp").addClass("clicked-button");
+// 	$(".current-sponsors").removeClass("hidden");
+// 	$(".previous-sponsors").addClass("hidden");
+// });
+
 $("#prev-sp").click(function(){
 	$("#curr-sp").removeClass("clicked-button");
-	$("#previous-sponsors").removeClass("hidden");
 	$("#prev-sp").addClass("clicked-button");
+	$("#previous-sponsors").removeClass("hidden");
 	$("#current-sponsors").addClass("hidden");
 });
 
 $("#curr-sp").click(function(){
-	$("#current-sponsors").removeClass("hidden");
 	$("#prev-sp").removeClass("clicked-button");
 	$("#curr-sp").addClass("clicked-button");
+	$("#current-sponsors").removeClass("hidden");
 	$("#previous-sponsors").addClass("hidden");
 });
