cec4365c00afa71bc2c3378a8f1dfeba06d6c7df
nis130 <nishitlimbani130@gmail.com>
2021-01-13 11:44:52 +0530

    Added in-view.js file and made changes in particle_g.js

--- assets/js/particle_g.js
+++ assets/js/particle_g.js
@@ -1,13 +1,20 @@
-$(document).ready(function() {
+// To make this work
+// Add class section and provide data-background-color attribute to the tags
+// link the in-view.js file to the html file
+
+var color = '#0ff'
+inView('.section').on('enter', function(el){
+  color = $(el).attr('data-background-color');
+  $('#particle-g').particleground('destroy');
   $('#particle-g').particleground({
-    dotColor: '#0ff',
+    dotColor: color,
     lineColor: '#ffffff',
 	minSpeedX: 0.05,
 	minSpeedY: 0.05,
 	maxSpeedX: 0.05,
 	maxSpeedY: 0.05,
 	density: 5000,
-	particleRadius: 1	,
+	particleRadius: 5,
 	lineWidth: 0.15,
 	proximity: 70,
 	parallaxMultiplier: 35
