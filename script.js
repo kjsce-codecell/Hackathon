022492855e826636f7ee0577401a2d62945e53c3
twilight-warlock <devansh.ts@somaiya.edu>
2021-01-13 14:20:51 +0530

    Fixed FAQ issue

---
+++ script.js
@@ -0,0 +1,21 @@
+for (let j = 1; j <=6 ; j++) {
+    document.querySelector(`#q${j} .q`).addEventListener("click",()=>{
+        document.querySelector(`#a${j}`).classList.toggle("open");
+        document.querySelector(`#q${j} .fa`).style.transform = "rotate(180deg)";
+        document.querySelector(`#q${j} .fa`).style.transition = "transform 0.3s";
+        if (document.querySelector(`#a${j}`).classList.contains("open")) {
+            document.querySelector(`#q${j} .fa`).classList.remove("fa-plus");
+            document.querySelector(`#q${j} .fa`).classList.add("fa-minus");
+        }else{
+            document.querySelector(`#q${j} .fa`).classList.remove("fa-minus");
+            document.querySelector(`#q${j} .fa`).classList.add("fa-plus");
+        }
+        for (let i = 1; i <= 6; i++) {
+            if(i!=j){
+                document.querySelector(`#a${i}`).classList.remove("open");
+                document.querySelector(`#q${i} .fa`).classList.remove("fa-minus");
+                document.querySelector(`#q${i} .fa`).classList.add("fa-plus");
+            }
+        }
+    })
+}
