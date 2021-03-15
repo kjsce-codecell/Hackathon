4c15fffbbc2612f1bdfeb2ddc8431e8e1a62b438
Naman Shah <nashah2001@gmail.com>
2021-03-16 01:50:57 +0530

    added few sponsors

--- assets/js/sponsors.js
+++ assets/js/sponsors.js
@@ -1,17 +1,3 @@
-// $("#prev-sp").click(function(){
-// 	$("#curr-sp").removeClass("clicked-button");
-// 	$("#prev-sp").addClass("clicked-button");
-// 	$("#previous-sponsors").removeClass("hidden");
-// 	$("#current-sponsors").addClass("hidden");
-// });
-
-// $("#curr-sp").click(function(){
-// 	$("#prev-sp").removeClass("clicked-button");
-// 	$("#curr-sp").addClass("clicked-button");
-// 	$("#current-sponsors").removeClass("hidden");
-// 	$("#previous-sponsors").addClass("hidden");
-// });
-
 $("#prev-sp").click(function(){
 	$("#curr-sp").removeClass("clicked-button");
 	$("#curr-sp").addClass("btn-effect");
