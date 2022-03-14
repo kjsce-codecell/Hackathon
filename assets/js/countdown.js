c449031fa00b9d74c00e37264363fd6ad9f15663
Arya Nair <aryaajitnair@gmail.com>
2022-03-14 18:08:17 +0530

    Uncomment countdown (#50)

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -2,13 +2,15 @@
 
 function countdown(){
     var now = new Date();
-    var eventDate = new Date(2022, 2, 2,19,0,0);
+    var eventDate = new Date(2022, 2, 16,19,0,0);
     var diff=eventDate-now;
-    var hours=Math.floor(diff/(60*60*1000));
+    var hours=Math.floor(diff/(60*60*1000))%24;
+    var days=Math.floor(diff/(60*60*1000*24));
     var minutes=Math.floor((diff%(60*60*1000))/(60*1000));
     var seconds=Math.floor((diff%(60*1000))/1000);
     document.getElementById("hours").innerHTML=hours;
     document.getElementById("minutes").innerHTML=minutes;
     document.getElementById("seconds").innerHTML=seconds;
+    document.getElementById("days").innerHTML=days;
 }
 setInterval(countdown, 1000);
