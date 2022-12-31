c0cf92fef3731269f15b8c81f4ea1ebb397d5e05
Gaurish <gaurishbaliga@gmail.com>
2022-12-31 13:10:04 +0530

    make Swiper responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
 $("img").attr("draggable", "false");
 
+
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+
+    slidesPerView: 1,
     spaceBetween: 30,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+         slidesPerView: 1,
+         spaceBetween: 20
+        },
+        // when window width is >= 480px
+        600: {
+         slidesPerView: 2,
+         spaceBetween: 30
+        },
+        // when window width is >= 640px
+        900: {
+         slidesPerView: 3,
+         spaceBetween: 40
+        }
+       },
     autoplay: {
       delay: 2500,
       disableOnInteraction: false
@@ -12,4 +31,5 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true
     }
   });
+
 
