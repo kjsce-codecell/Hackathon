08015dc086e491ebe83a4f4d3dbb2b55f656fae4
Gaurish <gaurishbaliga@gmail.com>
2022-12-31 12:54:44 +0530

    clean Team Card Code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 $("img").attr("draggable", "false");
 
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+  });
 
