33165e385f7f82380876729ad2f053a641978ef3
hailhydra1 <pranaysingh303@gmail.com>
2023-01-05 00:37:45 +0530

    add slider card animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,4 +33,42 @@ var swiper = new Swiper(".mySwiper", {
     }
   });
 
+var demoButtons;
+
+function start () {
+  
+  // Add event "click" to "demo buttons"
+  demoButtons = document.querySelectorAll ('.js-modify');
+  for (var i = 0; i < demoButtons.length; i++) {
+    demoButtons[i].addEventListener ('click', toggleEffect);
+  }
+  
+  // Add event "click" to "save buttons"
+  var saveButtons = document.querySelectorAll ('.js-save');
+  for (var i = 0; i < saveButtons.length; i++) {
+    saveButtons[i].addEventListener ('click', toggleActive);
+  }
+  
+}
+
+// Toggle "effect" classes
+function toggleEffect () {
+  var target = document.querySelector (this.dataset.target);
+      target.dataset.effect = this.dataset.effect;
+  
+  for (var i= 0; i < demoButtons.length; i++) {
+    demoButtons[i].classList.remove ('active');
+  }
+  
+  toggleActive.call (this);
+}
+
+// Toggle "active" class
+function toggleActive () {
+  this.classList.toggle ('active');
+}
+
+// Invoke "start ()" function
+window.addEventListener ('load', start);
+
 
