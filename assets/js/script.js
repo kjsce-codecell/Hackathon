eb8b509d656b952b391eaa024a22bb6e6fb0fb77
Pargat-Dhanjal <pargat14@hotmail.com>
2023-01-10 23:48:02 +0530

    fix: preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,52 +4,54 @@ AOS.init({
 });
 
 $("img").attr("draggable", "false");
-console.log("I'm Batman.")
+console.log("I'm Batman.");
 
-// Card Swiper 
+// Card Swiper
 
 var swiper = new Swiper(".mySwiper", {
-  
   autoHeight: true,
-    slidesPerView: 1,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-         slidesPerView: 2,
-         spaceBetween: 20,
-         slidesPerGroup: 2
-        },
-        // when window width is >= 480px
-        600: {
-         slidesPerView: 3,
-         spaceBetween: 30,
-         slidesPerGroup: 3
-        },
-        // when window width is >= 640px
-        900: {
-         slidesPerView: 4,
-         spaceBetween: 40,
-         slidesPerGroup: 4
-        }
-       },
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: true
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      slidesPerGroup: 2,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    }
-  });
-
-$(".mySwiper").hover(function() {
-    (this).swiper.autoplay.stop();
-}, function() {
-    (this).swiper.autoplay.start();
+    // when window width is >= 480px
+    600: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+    },
+    // when window width is >= 640px
+    900: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      slidesPerGroup: 4,
+    },
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
+
+$(".mySwiper").hover(
+  function () {
+    this.swiper.autoplay.stop();
+  },
+  function () {
+    this.swiper.autoplay.start();
+  }
+);
 swiper.autoplay.stop();
 
 // Stats numbers
@@ -68,19 +70,23 @@ var observer = new IntersectionObserver(
         $(".stat_number").each(function () {
           $(this)
             .prop("Counter", 0)
-            .animate({
-              Counter: $(this).text(),
-            }, {
-              duration: 3000,
-              easing: "swing",
-              step: function (now) {
-                $(this).text(Math.ceil(now));
+            .animate(
+              {
+                Counter: $(this).text(),
               },
-            });
+              {
+                duration: 3000,
+                easing: "swing",
+                step: function (now) {
+                  $(this).text(Math.ceil(now));
+                },
+              }
+            );
         });
       }
     });
-  }, {
+  },
+  {
     threshold: 0,
     rootMargin: "-200px 1500px -200px 1500px",
   }
@@ -91,5 +97,5 @@ observer.observe(stats);
 // Disable parallax on mobile
 
 if ($(window).width() < 768) {
-  $('.building').removeClass('object');
-};
+  $(".building").removeClass("object");
+}
