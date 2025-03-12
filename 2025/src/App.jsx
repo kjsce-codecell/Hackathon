5c3202076b89242e0a1dd98794278545ffd94d5c
shaurya2k06 <shaurya2k06@gmail.com>
2025-03-13 02:57:01 +0530

    hogya bhai

--- 2025/src/App.jsx
+++ 2025/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react"
-import Hero from "./sections/hero"
+import Hero from "./sections/hero.jsx"
+import Footer from "./components/footer/footer.jsx"
 function App() {
   return (
     <div className="">
       <Hero />
+      <Footer />
     </div>
   )
 }
