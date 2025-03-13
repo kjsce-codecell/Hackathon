4d607ba3c3ea3d5c5eae4b216fad0754ee3aaeb6
Aditya Belgaonkar <adityabelgaonkar.30@gmail.com>
2025-03-14 01:59:49 +0530

    flashback border.

--- 2025/src/sections/flashback.jsx
+++ 2025/src/sections/flashback.jsx
@@ -15,13 +15,14 @@ const FlashBack = () => {
       </header>
 
       <div className="flashback-video">
-        <div className="iframe-container">
-          <iframe
-            src="https://www.youtube.com/embed/MsIwzZziLfs"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          ></iframe>
+      <div className="iframe-container">
+        <iframe
+          src="https://www.youtube.com/embed/MsIwzZziLfs"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <img src="/assets/flashback-border.svg" className="iframe-border" />
         </div>
       </div>
     </section>
