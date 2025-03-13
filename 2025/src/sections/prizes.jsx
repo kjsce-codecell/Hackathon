d67345222f69423babbf80f65f49c3489b743ca4
tkshsbcue <kumar.tanay@somaiya.edu>
2025-03-13 19:35:34 +0530

    added background and made prizes text align

--- 2025/src/sections/prizes.jsx
+++ 2025/src/sections/prizes.jsx
@@ -7,14 +7,16 @@ const Prizes = () => {
             <img src="/assets/AboutUsLeftStreak.svg" className="PrizesLeftStreak" />
             <img src="/assets/AboutUsRightStreak.svg" className="PrizesRightStreak" />
             <div className="prizes-container">
-                <h2 className="prizes-title">Prizes</h2>
+                <header className="prizes-header">
+                    <h1>PRIZES</h1>
+                </header>
                 <div className="prizes-grid">
                     <div className="prize-card">
                         <div className="prize-category">
                             Cybersecurity
                         </div>
                         <div className="prize-image-placeholder">
-                            {/* Prize image or icon can be added here */}
+
                         </div>
                         <div className="prize-position">Winner</div>
                     </div>
