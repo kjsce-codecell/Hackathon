3f333fb1796df5bcbc25bf916b0071a69c824534
Aditya Belgaonkar <59801604+adityabelgaonkar05@users.noreply.github.com>
2025-03-14 21:05:05 +0530

    About flashbacks (#84)
    * add about and flashbacks.
    
    * fix about.
    
    * update assets.
    
    * update iframe css.
    
    * fix streak size, flashback padding, about text.
    
    * fix borders.
    
    * flashback border.
    
    * fix font size headers.
    
    * fix gradient, header size, flashback padding, min height.
    
    * minor font-size changes.
    
    ---------
    
    Co-authored-by: shaurya2k06 <shaurya2k06@gmail.com>

--- 2025/src/sections/flashback.jsx
+++ 2025/src/sections/flashback.jsx
@@ -15,25 +15,15 @@ const FlashBack = () => {
       </header>
 
       <div className="flashback-video">
-
+      <div className="iframe-container">
         <iframe
-          width="610"
-          height="385"
           src="https://www.youtube.com/embed/MsIwzZziLfs"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
-
-        {/* <div className="iframe-container">
-          <iframe
-            src="https://www.youtube.com/embed/MsIwzZziLfs"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          ></iframe>
-        </div> */}
-
+        <img src="/assets/flashback-border.svg" className="iframe-border" />
+        </div>
       </div>
     </section>
   );
