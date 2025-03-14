20a4d10d4a422a16d934279a6eb6534aaf853b32
Shreyans T <shreyans.tatiya@gmail.com>
2025-03-14 22:44:20 +0530

    feat: add new sponsor images for current and previous sponsors and display it in sponsor box

---
+++ 2025/public/sponsor/currsponsors.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        title: "Title Sponsor",
+        sponsors: [
+            {
+                src: "https://www.gajshield.com/",
+                img: "/sponsor/currentsponsors/GajshieldWhite.png"
+            }
+        ]
+    },
+    {
+        title: "Bronze Sponsor",
+        sponsors: [
+            {
+                src: "https://incred.email",
+                img: "/sponsor/currentsponsors/IncredLight.svg"
+            }
+        ]
+    },
+    {
+        title: "Platform Partner",
+        sponsors: [
+            {
+                src: "https://devfolio.co/",
+                img: "/sponsor/currentsponsors/devfolio.png"
+            }
+        ]
+    },
+    {
+        title: "Bronze Partner",
+        sponsors: [
+            {
+                src: "https://ethindia.co/",
+                img: "/sponsor/currentsponsors/EthIndia.png"
+            }
+        ]
+    },
+    {
+        title: "Domain Partner",
+        sponsors: [
+            {
+                src: "https://gen.xyz/",
+                img: "/sponsor/currentsponsors/xyz.png"
+            }
+        ]
+    },
+    {
+        title: "Entertainment Partner",
+        sponsors: [
+            {
+                src: "https://smaaash.in/",
+                img: "/sponsor/currentsponsors/smaaash.png"
+            }
+        ]
+    }
+]
